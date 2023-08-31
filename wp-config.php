744c50d3ed56bec8f11fdcce3e8fba9d471b7989
Skencker <Sandrinekencker@hotmail.com>
2023-08-31 09:10:57 +0200

    2

--- wp-config.php
+++ wp-config.php
@@ -21,16 +21,16 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'local' );
+define( 'DB_NAME', 'vez3yvf8usry1jtq' );
 
 /** Database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'zfum7wsaqzdiy48a' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'root' );
+define( 'DB_PASSWORD', 'zf2fox4dy0s5zi7v' );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'clwxydcjair55xn0.chr7pe7iynqr.eu-west-1.rds.amazonaws.com' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
